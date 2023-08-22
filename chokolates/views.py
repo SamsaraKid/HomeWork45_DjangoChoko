96e36a15f3811d2faab4ff4f3b6f7c8efffa9a51
korolevaj <koroliev@yandex.ru>
2023-08-22 16:43:41 +0300

    Initial commit

---
+++ chokolates/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import os
+
+# Create your views here.
+curdir = os.getcwd()
+
+
+def index(req):
+    return render(req, 'index.html')
+
+
+def chokolate(req, id):
+    id = int(id)
+    file = open(curdir + '/chokolates/static/text/content', 'r', encoding='utf-8')
+    chokoinfo = list(map(lambda x: list(x.split(' # ')), file.readlines()))
+    file.close()
+    name, img, text, link = chokoinfo[id]
+    return render(req, 'choko.html', context={'id': id, 'name': name, 'img': img, 'text': text, 'link': link})
